2cbbcacad5c35622fef13e1bf910ab15143ab617
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-07 20:08:09 -0300

    Update CrearClienteHandlerTests.cs

--- tests/ClientManagerAPI.Tests/Application/UseCases/CrearClienteHandlerTests.cs
+++ tests/ClientManagerAPI.Tests/Application/UseCases/CrearClienteHandlerTests.cs
@@ -1,1 +1,32 @@
 
+using Xunit;
+using FluentAssertions;
+using ClientManagerAPI.Application.UseCases.CrearCliente;
+using ClientManagerAPI.Application.DTOs;
+using ClientManagerAPI.Application.Services;
+using ClientManagerAPI.Tests.Mocks;
+
+public class CrearClienteHandlerTests
+{
+    [Fact]
+    public async Task Handle_Deberia_CrearClienteYDevolverId()
+    {
+        var dto = new ClienteDto
+        {
+            Nombre = "Ana",
+            Email = "ana@demo.com",
+            Calle = "Calle 1",
+            Ciudad = "Ciudad X",
+            Provincia = "Provincia Y"
+        };
+
+        var unitOfWork = UnitOfWorkMock.Create();
+        var service = new ClienteService();
+        var handler = new CrearClienteHandler(unitOfWork.Object, service);
+
+        var id = await handler.Handle(dto);
+
+        id.Should().NotBeEmpty();
+        unitOfWork.Verify(u => u.GuardarCambiosAsync(), Times.Once);
+    }
+}
