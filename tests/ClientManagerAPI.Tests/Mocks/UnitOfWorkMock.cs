2cefeaabd30c4e170b103c4d915de3769c84bcb7
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-07 20:08:54 -0300

    Update UnitOfWorkMock.cs

--- tests/ClientManagerAPI.Tests/Mocks/UnitOfWorkMock.cs
+++ tests/ClientManagerAPI.Tests/Mocks/UnitOfWorkMock.cs
@@ -1,1 +1,20 @@
 
+using Moq;
+using ClientManagerAPI.Application.Interfaces;
+using ClientManagerAPI.Domain.Interfaces;
+
+namespace ClientManagerAPI.Tests.Mocks;
+
+public static class UnitOfWorkMock
+{
+    public static Mock<IUnitOfWork> Create()
+    {
+        var repoMock = new Mock<IClienteRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+        unitOfWorkMock.Setup(u => u.Clientes).Returns(repoMock.Object);
+        unitOfWorkMock.Setup(u => u.GuardarCambiosAsync()).ReturnsAsync(1);
+
+        return unitOfWorkMock;
+    }
+}
